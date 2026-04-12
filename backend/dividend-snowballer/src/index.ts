9e32b4164f93b5a3a90eff088eb8976c371bee95
Unnar <89518256+Sethclone@users.noreply.github.com>
2026-04-12 19:33:17 +0000

    oh no i pushed .env to main ;-; fixed index to not have specic port, added prisma to package and also prisma scripts for deployment on railway

--- backend/dividend-snowballer/src/index.ts
+++ backend/dividend-snowballer/src/index.ts
@@ -19,9 +19,11 @@ app.route('/api/stocks', stocks)
 
 app.get('/', (c) => c.text('Dividend Snowballer API'))
 
+const port = Number(process.env.PORT) || 3000
+
 serve({
   fetch: app.fetch,
-  port: 3000,
+  port,
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
